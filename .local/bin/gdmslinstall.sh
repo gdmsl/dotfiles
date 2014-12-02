b0537eadb6ac36125132f7716267a4bee9cdb7dd
Guido Masella <guido.masella@gmail.com>
2014-12-02 11:21:25 +0100

    maliwan color scheme in installation

--- .local/bin/gdmslinstall.sh
+++ .local/bin/gdmslinstall.sh
@@ -15,16 +15,26 @@ if [ ! -d vundle ]; then
 	git clone http://github.com/gmarik/vundle
 fi
 
-echo "Installing all boundles from .vimrc"
-vim -c 'BundleInstall'
-
 echo "Changing working directory to $HOME"
 cd $HOME
+
+echo "Installing all boundles from .vimrc"
+vim -c 'BundleInstall'
 
 echo "Installing ttytter-extension"
 git clone https://github.com/oxhak/ttytter-extension ~/.ttytter/ttytter-extension
 
 echo "Installing OHMYZSH"
-echo "########################################################"
-curl -L http://install.ohmyz.sh | sh
-echo "########################################################"
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+echo "Installing base16-builder"
+git clone git://github.com/chriskempson/base16-builder.git ~/.base16
+
+echo "Installing Maliwan Color Scheme"
+cd ~/.base16
+./base16 $DOTFILES/maliwan.yml
+bash ~/.base16/output/guake/base16-maliwan.dark.sh
+bash ~/.base16/output/gnome-terminal/base16-maliwan.dark.sh
+cp -f ~/.base16/output/xresources/base16-maliwan.dark.xresources ~/.xrdb/base16-maliwan.dark.xrdb
+
+
