1190d62dc6580064856fe1e713bff1e7c736c136
Guido Masella <guido.masella@gmail.com>
2015-02-24 15:23:31 +0100

    added configurations for julia

---
+++ julia/.juliarc.jl
@@ -0,0 +1,6 @@
+#
+# AUTHOR: Guido Masella <guido.masella@gmail.com>
+#
+
+push!(LOAD_PATH, "$(ENV["HOME"])/Develop/julia/modules/YAODES")
+push!(LOAD_PATH, "$(ENV["HOME"])/Develop/julia/modules/Rydberg")
