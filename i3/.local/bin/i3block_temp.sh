41e48c593cd335abb3106cb72d77cce11ffce033
Guido Masella <guido.masella@gmail.com>
2015-12-12 10:44:41 +0100

    qop added

--- i3/.local/bin/i3block_temp.sh
+++ i3/.local/bin/i3block_temp.sh
@@ -3,11 +3,7 @@
 # AUTHOR: Guido Masella <guido.masella@gmail.com>
 #
 
-if [ "$HOSTNAME" = "rubick" ]; then
-    temp=`sensors coretemp-isa-0000 | awk '/Physical/ { print $4 }' | grep -m 1 --color=none -o "[0-9]\{1,\}" - | head -n 1`
-elif [ "$HOSTNAME" = "spectre" ]; then
-    temp=`sensors coretemp-isa-0000 | awk '/Physical/ { print $4 }' | grep -m 1 --color=none -o "[0-9]\{1,\}" - | head -n 1`
-fi
+temp=`sensors coretemp-isa-0000 | awk '/Physical/ { print $4 }' | grep -m 1 --color=none -o "[0-9]\{1,\}" - | head -n 1`
 
 bef='<span  letter_spacing="-4800" rise="00">'
 aft='</span>'
