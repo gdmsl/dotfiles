9d17f8ba7ea302979bc6eef425818d2dbc1358ec
Guido Masella <guido.masella@gmail.com>
2016-01-19 10:22:15 +0100

    sup changes to hooks

--- mail/.sup/hooks/before-poll.rb
+++ mail/.sup/hooks/before-poll.rb
@@ -1,6 +1,6 @@
 if (@last_fetch || Time.at(0)) < Time.now - 120
   say "Running offlineimap..."
-  system "offlineimap -o -u quiet 2>&1"
+  system "offlineimap -o -u quiet &> /dev/null"
   say "Finished offlineimap run."
   @last_fetch = Time.now
 end
