54514e147cf90da7600ccdbecb971b205850ed5d
Guido Masella <guido.masella@gmail.com>
2016-12-05 19:31:17 +0100

    updated mail config

--- mail/.local/bin/offlineimap.py
+++ mail/.local/bin/offlineimap.py
@@ -2,4 +2,4 @@
 from subprocess import check_output
 
 def get_pass():
-    return check_output("gpg -dq ~/Documents/Identity/msmtp-gmail.gpg", shell=True).strip("\n")
+    return check_output("gpg -dq ~/usr/documents/id/msmtp-gmail.gpg", shell=True).strip("\n")
