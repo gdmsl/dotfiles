dfb5f4e5d5695e9c34e8210628ce2ef912e14b5c
Guido Masella <guido.masella@gmail.com>
2018-02-20 17:08:20 +0100

    revamped emails

--- astroid/.config/astroid/poll.sh
+++ astroid/.config/astroid/poll.sh
@@ -9,14 +9,31 @@
 # located at ~/.config/astroid/poll.sh
 #
 
+MAILDIR="$HOME/var/mail"
+
 # Exit as soon as one of the commands fail.
 set -e
+
+# check if we have a connection
+if ! ping -w 1 -W 1 -c 1 mail.google.com; then
+    echo "there is no internet connection"
+    exit
+fi
+
+# check if .mail is mounted
+if [ ! -d $MAILDIR ]; then
+    echo "$MAILDIR does not seem to be mounted"
+    exit
+fi
 
 # Fetch new mail.
 mbsync -a
 
 # Import new mail into the notmuch database.
 notmuch new
+
+# Run afew tag
+afew --new
 
 # Here you can process the mail in any way you see fit. See the following link
 # for examples:
