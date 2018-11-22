4b1b165eaf7be1620455fd19d18320502a1d6707
Guido Masella <guido.masella@gmail.com>
2018-11-22 12:03:20 +0100

    More bars

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,7 +6,18 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch for the current machine
-polybar $HOSTNAME &
+# Get the connected monitors
+monitors=$(xrandr | grep " connected" | awk '{ print $1 }')
+
+counter=1
+for m in $monitors; do
+    postfix="$counter"
+    if [ $counter -eq 1 ]; then
+        postfix=''
+    fi
+    polybar $HOSTNAME$postfix &
+
+    counter=$((counter+1))
+done
 
 echo "Bars launched..."
