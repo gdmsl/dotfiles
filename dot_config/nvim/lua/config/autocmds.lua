69869449497bbb9df79d3dde5bbeadc1702eca56
Guido Masella <guido.masella@gmail.com>
2023-04-18 17:22:51 +0200

    misc: updates

---
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,10 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "julia", "python" },
+  callback = function()
+    vim.opt.shiftwidth = 4
+  end,
+})
--- dot_julia/config/startup.jl
+++ dot_julia/config/startup.jl
@@ -2,7 +2,18 @@
 
 # use revise by default
 try
-    using Revise
+  using Revise
 catch e
-    @warn "Error initializing Revise" exception=(e, catch_backtrace())
+  @warn "Error initializing Revise" exception = (e, catch_backtrace())
+end
+
+atreplinit() do repl
+  try
+    @eval begin
+      using Logging: global_logger
+      using TerminalLoggers: TerminalLogger
+      global_logger(TerminalLogger())
+    end
+  catch
+  end
 end
