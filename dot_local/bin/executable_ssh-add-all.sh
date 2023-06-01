110dddef1ad97998b65266f0a4953d31c138c8fa
Guido Masella <guido.masella@gmail.com>
2023-06-01 17:56:42 +0200

    fix: change order of reading ssh keys

--- dot_local/bin/executable_ssh-add-all.sh
+++ dot_local/bin/executable_ssh-add-all.sh
@@ -2,7 +2,16 @@
 
 cd $HOME/.ssh
 
-for key in ${HOME}/.ssh/id_*.pub; do
-    ssh-add ${key/.pub/} < /dev/null
+for key in ${HOME}/.ssh/id_*.${HOSTNAME}.pub; do
+	echo "Adding $key"
+	ssh-add ${key/.pub/} < /dev/null
 done
 
+for key in ${HOME}/.ssh/id_*.pub; do
+	if echo "$key" | grep ${HOSTNAME} >/dev/null; then
+		continue
+	fi
+
+	echo "Adding $key"
+	#ssh-add ${key/.pub/} < /dev/null
+done
