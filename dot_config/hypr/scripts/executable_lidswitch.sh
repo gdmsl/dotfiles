0ccc64d1333740341d5da08dcd53fac8ccf51d99
Guido Masella <guido.masella@gmail.com>
2025-04-17 11:14:32 +0200

    update

--- dot_config/hypr/scripts/executable_lidswitch.sh
+++ dot_config/hypr/scripts/executable_lidswitch.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# check the status of the lid
 if grep open /proc/acpi/button/lid/LID0/state; then
-	hyprctl keyword monitor "eDP-1, 1920x1080, 0x0, 1"
-	~/.config/hypr/scripts/monitor-config.sh
+    kanshictl switch home || kanshictl switch presenter
 else
-	if [[ $(hyprctl monitors | grep -c "Monitor") != 1 ]]; then
-		hyprctl keyword monitor "eDP-1, disable"
-	fi
+    kanshictl switch home-nolid || kanshictl switch presenter-nolid
+
+    if [ $? -eq 1 ]; then
+        sleep 1
+        hyprlock
+    fi
 fi
-
-# sleep to avoid bad picture in swaylock
-sleep 1
-
-# lock the screen
-swaylock
