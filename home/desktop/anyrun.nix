ba630d2b26e1ddd1bf9a1911d67da6fab93a9e40
Guido Masella <guido.masella@gmail.com>
2026-03-23 03:56:31 +0100

    fix: remove duplicate anyrun HM module import
    Already declared in home-manager's built-in modules.

--- home/desktop/anyrun.nix
+++ home/desktop/anyrun.nix
@@ -1,21 +1,17 @@
 { inputs, pkgs, ... }:
 
 {
-  imports = [
-    inputs.anyrun.homeManagerModules.default
-  ];
-
   programs.anyrun = {
     enable = true;
     config = {
       plugins = with inputs.anyrun.packages.${pkgs.stdenv.hostPlatform.system}; [
-        applications   # app launcher
-        shell          # shell commands
-        symbols        # unicode symbols
-        translate      # translation
-        dictionary     # dictionary lookup
-        websearch      # web search
-        kidex          # file search
+        applications
+        shell
+        symbols
+        translate
+        dictionary
+        websearch
+        kidex
       ];
       width.fraction = 0.3;
       hidePluginInfo = true;
