985e8f599c185004b36815c25f1763ba28c165fe
Guido Masella <guido.masella@gmail.com>
2026-04-12 21:45:26 +0200

    fix: use backupFileExtension instead of force for .gtkrc-2.0
    The force override on home.file.".gtkrc-2.0" conflicts with the gtk
    module's internal key for the same file. Use the global
    backupFileExtension option to handle existing file conflicts instead.

--- flake.nix
+++ flake.nix
@@ -42,6 +42,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "hm-backup";
             home-manager.extraSpecialArgs = { inherit inputs; };
             home-manager.users.gdmsl = import ./home;
           }
