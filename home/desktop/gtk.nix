93f8aa109fdc5617206bd7839dafac5dcbef3f24
Guido Masella <guido.masella@gmail.com>
2026-04-12 21:04:06 +0200

    fix: remove conflicting .gtkrc-2.0 force override
    The explicit home.file.".gtkrc-2.0".force entry conflicts with the
    gtk module's own management of that file.

--- home/desktop/gtk.nix
+++ home/desktop/gtk.nix
@@ -46,7 +46,6 @@
   xdg.configFile."gtk-3.0/settings.ini".force = true;
   xdg.configFile."gtk-4.0/settings.ini".force = true;
   xdg.configFile."gtk-4.0/gtk.css".force = true;
-  home.file.".gtkrc-2.0".force = true;
   xdg.dataFile."icons/default/index.theme".force = true;
 
   dconf.settings = {
