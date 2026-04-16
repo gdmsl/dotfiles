594267582d117c1cd0a44f5c3786724e0d982884
Guido Masella <guido.masella@gmail.com>
2026-04-16 10:06:07 +0200

    fix: add libsqlite to neovim LD_LIBRARY_PATH for yanky.nvim

--- home/editor/neovim.nix
+++ home/editor/neovim.nix
@@ -6,6 +6,9 @@
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
+    extraWrapperArgs = [
+      "--suffix" "LD_LIBRARY_PATH" ":" "${pkgs.sqlite.out}/lib"
+    ];
   };
 
   # Deploy LazyVim config via symlink so lazy.nvim can write to the directory
